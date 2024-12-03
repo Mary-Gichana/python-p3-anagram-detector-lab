e62f612c823cd82455656967f9d1f4e0f7409b92
Mary Monchari Gichana <marygichana95@gmail.com>
2024-12-03 18:51:10 +0300

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+    
+        self.word = word.lower()
+
+    def match(self, words):
+        
+        
+        sorted_word = sorted(self.word)
+        
+        
+        return [
+            word for word in words
+            if word.lower() != self.word and sorted(word.lower()) == sorted_word
+        ]
